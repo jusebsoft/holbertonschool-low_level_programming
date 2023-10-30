a187450306200ab5618bae36cb5040845ea8780d
jusebsoft <juansebastiang867@gmail.com>
2023-10-30 10:35:01 -0700

    comando printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - punto de entrada funcion main
+  * 
+  * Description: 'usar la funcion printf psra mostrar el mensaje'
+  * @parameter: funcion printf tipo de dato caracter
+  * 
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+printf("\"with proper grammar, but the outcome is a piece of art,");	    
+return (0);
+}
