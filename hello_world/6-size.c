72ee501a84fa66f56241d21a4b79cc6e935abb7c
jusebsoft <juansebastiang867@gmail.com>
2023-10-30 13:21:49 -0700

    fix 5 impresion de varios tipos de datos

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -4,7 +4,7 @@
 * main - Prints the size of various types based on
 * the computer it is compiled and run on..
 * descripcion: imprimir el tamano de varios tipos de datos
-* parametro:datos enteros,caracteres,largos y flotantes 
+* parametro: datos enteros caracteres largos y flotantes 
 * Return: Always 0 
 */
 
