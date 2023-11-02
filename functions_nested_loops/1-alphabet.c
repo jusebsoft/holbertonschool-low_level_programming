6362d8217d9e877c97a8dfa944e0da083728fb7f
jusebsoft <juansebastiang867@gmail.com>
2023-11-02 15:16:28 -0700

    Add in my repository

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  *main - funcion para imprimir el alfabeto en minuscula
+  *Return: 0.
+  */
+
+void print_alphabet(void)
+{
+
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+
+	_putchar('\n');
+
+}
---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* void - print_alphabet_x10.c - impirmir el alfabeto 10 veces
+* Return: 0
+*/
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
+
+	for  (i = 0; i < 0; i++)
+	{
+		for (j = 'a'; j = 'z'; j++)
+			_putchar(j);
+		_putchar('\n');
+	}
+}
+
+
