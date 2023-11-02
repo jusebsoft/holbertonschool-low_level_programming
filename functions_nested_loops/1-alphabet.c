f7dab335f31f101cc6184e9e4b7eddeba1bc11d5
jusebsoft <juansebastiang867@gmail.com>
2023-11-02 15:49:51 -0700

    fix  1 functions

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-  *main - funcion para imprimir el alfabeto en minuscula
-  *Return: 0.
+  * print_alphabet - funcion para imprimir el alfabeto en minuscula
+  * Return: 0.
   */
 
 void print_alphabet(void)
