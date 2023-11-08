e5de311d62e6929d48f3001dc9b1c6953f4cbba5
jusebsoft <juansebastiang867@gmail.com>
2023-11-08 05:39:32 -0800

    imprimir numero mayor o menor de 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -10,38 +10,20 @@
 
 void print_to_98(int n)
 {
-	int i;
-
-	if (n >= 0 && n <= 98)
+	if (n >= 98)
 	{
-		for (i = n ; i <= 98 ; i++)
+		for (; n > 98; n--)
 		{
-			printf("%d", i);
-			if (i != 98)
-				printf(", ");
+			printf("%d, ", n);
 		}
-		printf("\n");
-	}
-	else if (n < 0)
-	{
-		for (i = n ; i <= 98 ; i++)
-		{
-			printf("%d", i);
-			if (i != 98)
-				printf(", ");
-		}
-		printf("\n");
-	}
-	else if (n > 98)
-	{
-		 for (i = n ; i >= 98 ; i--)
-		 {
-			 printf("%d", i);
-			 if (i != 98)
-				 printf(", ");
-		 }
-		 printf("\n");
-	}
-	else if (n == 98)
 		printf("%d\n", n);
+	}
+	else
+	{
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+		printf("%d\n", n);
+	}
 }
