d548638111558877c018975425e7ea83cdf85c13
jusebsoft <juansebastiang867@gmail.com>
2023-11-07 17:32:29 -0800

    imprimir el ultimo digito

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_last_digit - print last digit
+* @i: function parameter
+* Description: imprimir el ultimo digito entero
+* Return: j
+*/
+
+int print_last_digit(int i)
+{
+	int j;
+
+	j = i % 10;
+	if (i < 0)
+		k = -j;
+	_putchar(j - '0');
+	return (j);
+
+}
