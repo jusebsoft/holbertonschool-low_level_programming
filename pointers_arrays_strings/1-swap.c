4b68e6daba1e944b4fc41543acff4ea23b1d798e
jusebsoft <juansebastiang867@gmail.com>
2023-11-08 11:28:36 -0800

    tomar el valor de punteros a dos enteros

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* swap_int - swaps the values of two int
+* Description: funcion para tomar el valor de dos enteros
+* @a: parametro 1
+* @b: parametro 2
+* Return: a, b.
+*/
+
+void swap_int(int *a, int *b)
+{
+	int = m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+
+}
