cc0ec3f85b08d4a6f9e0dd743ef04b834efe4712
jusebsoft <juansebastiang867@gmail.com>
2023-11-08 12:37:42 -0800

    longitud de un string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _strlen - returns the length of a string
+* @s: string
+* Description: funcions para sacar la longitud de un caracter
+* Return: length.
+*/
+
+int _strlen(char *s)
+{
+	int longi = 0;
+	while (*s != '\0')
+
+	{
+		longi++;
+		s++;
+	}
+	return (longi);
+}
