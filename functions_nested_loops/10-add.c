64772da5d7f100002be20fc04374db785ac82a9f
jusebsoft <juansebastiang867@gmail.com>
2023-11-07 19:41:01 -0800

    suma de dos enteros

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* add - function for to sume two numbers
+* @a: parametro 1.
+* @b: parametro 2.
+* Description: se usa la funcion para sumar dos enteros
+* Return: a + b.
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
