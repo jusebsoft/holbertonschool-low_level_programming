54fd00fc4009826ae71f752950c4197edf8145bc
jusebsoft <juansebastiang867@gmail.com>
2023-11-07 17:07:31 -0800

    imprimir el valor absoluto de cada numero

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _abs - function to look the absolute value of a number
+* @a: function parameter
+* Description: se usa dos condicionales para obtener el valor absolut
+* Return: -a or a.
+*/
+
+int _abs(int a)
+{
+	if (a < 0)
+		return (-a);
+	else if
+	{
+		return (a);
+	}
+	return (0);
+}
