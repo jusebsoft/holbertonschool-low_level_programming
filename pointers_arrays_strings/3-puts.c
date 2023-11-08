d6618ee1a71a8a18bb673240c53c790cf91b7f0f
jusebsoft <juansebastiang867@gmail.com>
2023-11-08 12:50:55 -0800

    puntero *str para imprimir

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _puts - prints a string
+* @str: comando _puts
+* function: using to print
+* Return:
+*/
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	return ('\n');
+}
