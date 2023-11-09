c87e5c8764d02cdccfe16d677dd890e5e33bd311
jusebsoft <juansebastiang867@gmail.com>
2023-11-08 19:39:20 -0800

    imprimir un string en reversa

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -2,11 +2,25 @@
 
 /**
 * print_rev - prints an string in reverse
-* 
-*
+* @s: parametro string
+* Return: 0.
 */
 
 void print_rev(char *s)
 {
+	int longi = 0;
+	int o;
 
+	while (*s != '\0')
+	{
+		longi++
+		s++;
+	}
+	s--;
+	for (o = longi; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
 }
