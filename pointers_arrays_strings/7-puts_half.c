d18663e45bf2851d329e309ce599d1d6b710b6c7
jusebsoft <juansebastiang867@gmail.com>
2023-11-13 17:51:09 -0800

    print half by n

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* puts_half - prints half of a string, followed by a new line
+* @str: char
+* Return: print.
+*/
+
+void puts_half(char *str)
+{
+	char half = 0;
+
+	while (*str != '\0')
+	{
+		half++;
+		s++;
+	}
+	return (half);
+}
